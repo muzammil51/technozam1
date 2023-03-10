abde6f1f318f5a42a862955ac9f4bce17666f41d
Maham2k <92599955+Maham2k@users.noreply.github.com>
2023-03-10 11:14:14 +0500

    Unit Test

--- react-social/src/__tests__/Short.test.js
+++ react-social/src/__tests__/Short.test.js
@@ -75,3 +75,35 @@ test("render shortqs component",() => {
     var textele = screen.getAllByRole('link');
     expect(textele).toBeDefined()
 });
+
+
+test("render Home component",() => {
+    
+    render(<BrowserRouter>
+    <Home />
+    </BrowserRouter>);
+
+
+    var textele = screen.getAllByRole('button');
+    expect(textele).toHaveLength(1)
+});
+
+test("render shortqs component",() => {
+    
+    render(<BrowserRouter>
+    <Signup />
+    </BrowserRouter>);
+
+    var textele = screen.getAllByRole('button');
+    expect(textele).toHaveLength(1)
+});
+
+test("render shortqs component",() => {
+    
+    render(<BrowserRouter>
+    <Signup />
+    </BrowserRouter>);
+
+    var textele = screen.getAllByRole('link');
+    expect(textele).toBeDefined()
+});
