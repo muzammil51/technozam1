4220ccdd604dcd2f75de41274ee6bbf1f60c6243
Maham2k <92599955+Maham2k@users.noreply.github.com>
2023-03-31 10:56:34 +0500

    Updated App

--- Admin/react-hooks-frontend/src/App.js
+++ Admin/react-hooks-frontend/src/App.js
@@ -3,13 +3,22 @@ import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AppHeader from './common/AppHeader';
 // import HeaderComponent from './components/HeaderComponent';
 import ListUserComponent from './components/ListUserComponent';
+
 import AllUserComponent from './components/AllUserComponent';
+import AllReportComponent from './components/AllReportComponent';
+import AllContactComponent from './components/AllContactComponent';
+
 
 import ProfileComponent from './components/ProfileComponent';
 
 
 import AddUserComponent from './components/AddUserComponent';
 import ViewUserComponent from './components/ViewUserComponent';
+import ViewReportComponent from './components/ViewReportComponent';
+import ViewContactComponent from './components/ViewContactComponent';
+
+
+
 import AppFooter from './common/AppFooter';
 
 
@@ -26,14 +35,24 @@ function App() {
           <Switch>
               <Route exact path = "/" component = {ListUserComponent}></Route>
               <Route path = "/users" component = {ListUserComponent}></Route>
+
               <Route path = "/allusers" component = {AllUserComponent}></Route>
+              <Route path = "/allreports" component = {AllReportComponent}></Route>
+              <Route path = "/allcontacts" component = {AllContactComponent}></Route>
+
+
 
               <Route path = "/profile" component = {ProfileComponent}></Route>
 
 
               <Route path = "/add-user" component = {AddUserComponent} ></Route>
               <Route path = "/edit-user/:id" component = {AddUserComponent}></Route>
+
               <Route path = "/view-user/:id" component = {ViewUserComponent}></Route>
+              <Route path = "/view-report/:id" component = {ViewReportComponent}></Route>
+              <Route path = "/view-contact/:id" component = {ViewContactComponent}></Route>
+
+
             </Switch>
             <br></br>
         </div>
