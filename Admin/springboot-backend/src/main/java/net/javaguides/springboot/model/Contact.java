3d95db919b57bfc21369151189cbfdb0b347c08a
muzammil <116190052+muzammil51@users.noreply.github.com>
2023-03-31 08:43:43 +0500

    admin contact model

---
+++ Admin/springboot-backend/src/main/java/net/javaguides/springboot/model/Contact.java
@@ -0,0 +1,33 @@
+package net.javaguides.springboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "contacts")
+
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "subject")
+    private String subject;
+
+    @Column(name = "problem")
+    private String message;
+}
