bcb15a8c373dd31573b685333c7965c1b65585c9
MuzammilM1 <116190052+muzammil51@users.noreply.github.com>
2023-04-07 08:44:55 +0500

    Text History of each module models

---
+++ spring-social/src/main/java/com/example/springsocial/model/FillText.java
@@ -0,0 +1,34 @@
+package com.example.springsocial.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "filltexts")
+
+public class FillText {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "subject")
+    private String subject;
+
+    @Column(name = "input")
+    private String input;
+
+    @Column(name = "output")
+    private String output;
+
+    @Column(name = "timedate")
+    private String timedate;
+    
+}
